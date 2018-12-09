25c4bbe5958776c73e07b589558faaab14495cff
RayneDance <JordanLAlexander@gmail.com>
2018-12-09 01:28:24 -0600

    Added +# functionality to dice roll.

--- index.js
+++ index.js
@@ -23,35 +23,29 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
         args = args.splice(1);
         switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-			
+		
+		//Output available commands. We should clean this up.
 	    case 'help':
-			bot.sendMessage({
+				bot.sendMessage({
 				to:channelID,
 				message: 'Current commands: !roll #d#'
 			});
 	    break;
 
 
-            case 'roll':
+        case 'roll':
 				if(dice){
 					var result = rolldice(dice);
 					bot.sendMessage({
 					to: channelID,
 					message: result
-                });
- 		}else{
-  		bot.sendMessage({
-  			to: channelID,
- 			message: 'No dice value(IE: 1d6)'
- 		});
- } 
+					});
+						}else{
+							bot.sendMessage({
+							to: channelID,
+							message: 'No dice value(IE: 1d6)'
+							});
+						} 
  	   break;
  
  
@@ -64,47 +58,71 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 //Function to return a dice roll.
 function rolldice(dice){
 
-//So users can 1d6 or 1D6
-dice = dice.toUpperCase();
+	//So users can 1d6 or 1D6
+	dice = dice.toUpperCase();
 
-//split and setup vars
-var args = dice.split('D');
-var results = '';
-var total = 0;	
-	
-	//Leave function on poorly formatted request.
-	if(isNaN(Number(args[0])) ||
-	isNaN(Number(args[1])) ||
-	args.length === 1){
-		return 'Bad format';	
-	}
-
-	if(Number(args[0]) > 10){
-		return 'Cannot roll more than 10 dice';
-	}
-	
-	if(Number(args[1]) > 10000){
-		return 'Please submit a smaller number';
-	}
-
-	if(Number(args[0]) === 1){
-		results = Math.floor(Math.random() * args[1])+1;
-	}else{
-	
-		for(var i=0; i < Number(args[0]); i = i+1){
-			var hold = 0;
-			
-			if(i>0){
-				hold = Math.floor(Math.random() * args[1])+1;
-				total = total + hold;
-				results = results + ', '+ hold.toString(10);
-			}else{ 
-				results = Math.floor(Math.random() * args[1])+1;
-				total = results;
-			     }
+	//split and setup vars
+	var args = dice.split('D');
+	var sub = args[1].split('+');
+	var results = '';
+	var total = 0;	
+		
+		if(sub.length > 1){
+			args[1] = sub[0];
+		}else{sub = 0;}
+		
+		//Leave function on poorly formatted request.
+		if(isNaN(Number(args[0])) ||
+		isNaN(Number(args[1])) ||
+		args.length === 1){
+			return 'Bad format';	
 		}
-	results = 'Total: ' + total.toString(10) + ', rolls:' + results;
+	
+	//Minimizing rolls just to keep things clean
+		if(Number(args[0]) > 10){
+			return 'Cannot roll more than 10 dice';
+		}
+	
+		//Leave function on uselessly high numbers
+		if(Number(args[1]) > 10000){
+			return 'Please submit a smaller number';
+		}
+	
+	//If we're only rolling 1d#, handle it.
+		if(Number(args[0]) === 1){
+			results = "Rolled: " + (RandNumber(args[1], 1) + sub);
+		}else{
+			//handling for rolling more than 1 dice.
+			for(var i=0; i < Number(args[0]); i = i+1){
+				var hold = 0;
+			
+			// I want to clean this up. We're adding a lot
+			// of execution time here.
+			// The else is run first time through to avoid
+			// adding in a ', ' before the first roll
+				if(i>0){
+					hold = RandNumber(args[1]);
+					total = total + hold;
+					results = results + ', '+ hold.toString(10);
+				}else{ 
+					results = RandNumber(args[1]));
+					total = results;
+					 }
+		}
+	results = 'Total: ' + total.toString(10) + ', Rolls:' + results;
 	}
 	
 	return results;
+}
+
+function RandNumber(lim, str){
+	
+		str = str || false;
+		var randvalue = 0;
+		randvalue = Math.floor(Math.random() * lim)+1l
+		
+		if(str){
+			return randvalue.toString(10);
+		}
+		return randvalue;
 }
