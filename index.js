81fc2d38a0dfa9a77ede9cf8e2c3915066fc621c
Alexander Jordan <JordanLAlexander@gmail.com>
2018-12-08 19:59:01 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+//Index
+const express = require('express')
+const app = express;
+const port = 2727;
+
+app.get('/', (req, res) => res.send('hello world'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
