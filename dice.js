5c5b6fad5c36c19cd38c95cc740f20e4c7764602
Alexander Jordan <JordanLAlexander@gmail.com>
2018-12-10 16:47:19 +0000

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,5 +1,10 @@
 //Nodejs module for dice rolls
 
-exports.roll = function() {
-	console.log('Dice module test');
+//exports.roll = function() {
+//	console.log('Dice module test');
+//};
+module.exports = {
+	roll: function(){
+		console.log('Dice module test');
+	}
 };
